7e7d60ed1a381a240caba4efd72f470e595550ef
서정권 <sjg3234@gmail.com>
2023-10-17 15:56:10 +0900

    javascript

--- javascript-practices/basics/ex02.js
+++ javascript-practices/basics/ex02.js
@@ -27,6 +27,26 @@ console.log(s + ":" + typeof(s));
 console.log(b + ":" + typeof(b));
 
 console.log("=== 객체타입(object) ======")
+var i2 = new Number(10);
+var s2 = new String('hello');
+var b2 = new Boolean(true);
+var o = new Object();
+var a = new Array();
+var d = new Date();
 
 
-console.log("=== 객체타입(function) ======")
+console.log(i2 + ":" + typeof(i2) + ":" + (i2 instanceof(Number)));
+console.log(s2 + ":" + typeof(s2) + ":" + (s2 instanceof(String)));
+console.log(b2 + ":" + typeof(b2) + ":" + (b2 instanceof(Boolean)));
+console.log(o + ":" + typeof(o) + ":" + (o instanceof(Object)));
+console.log(a + ":" + typeof(a) + ":" + (a instanceof(Array)));
+console.log(d + ":" + typeof(d) + ":" + (d instanceof(Date)));
+
+// console.log("=== 객체타입(function) ======")
+
+
+// 유사 객체
+console.log("=== 원시 타입도 메소드를 호출 할 수 있다.(유사 객체) ======")
+console.log(b2.valueOf());
+console.log(b.valueOf()); // console.log(new Boolean(b).valueOf())
+console.log(new Boolean(b).valueOf());
